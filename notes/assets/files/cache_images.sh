bc100d55e4eb80c82a9c0e7915537b995f1afbb5
Steve Taylor <staylorx@gmail.com>
2022-09-16 09:14:12 -0600

    Update docker pull file

--- notes/assets/files/cache_images.sh
+++ notes/assets/files/cache_images.sh
@@ -8,52 +8,51 @@
 #
 # It has the added benefit of greatly increasing development velocity.
 #
-# tools and languanges
-podman pull sonatype/nexus3
-podman pull node
-podman pull python
-podman pull tiangolo/uwsgi-nginx-flask
-podman pull tiangolo/uvicorn-gunicorn-fastapi
-podman pull tiangolo/uvicorn-gunicorn-starlette
-podman pull tiangolo/meinheld-gunicorn-flask
-podman pull django
-podman pull docker
-podman pull centos:7
-podman pull centos
-podman pull alpine
+docker pull sonatype/nexus3
+docker pull node
+docker pull python
+docker pull tiangolo/uwsgi-nginx-flask
+docker pull tiangolo/uvicorn-gunicorn-fastapi
+docker pull tiangolo/uvicorn-gunicorn-starlette
+docker pull tiangolo/meinheld-gunicorn-flask
+docker pull django
+docker pull docker
+docker pull debian
+docker pull centos:8
+docker pull centos
+docker pull alpine
 
 # databases
-podman pull mongo
-podman pull adminer
-podman pull postgres:latest
-podman pull postgres:11
-podman pull mariadb
+docker pull mongo
+docker pull adminer
+docker pull postgres:latest
+docker pull mariadb
 
 # workflow and graphql
-podman pull prefecthq/server
-podman pull prefecthq/ui
-podman pull prefecthq/apollo
-podman pull hasura/graphql-engine
-podman pull dgraph/standalone:master
-podman pull dgraph/dgraph
+docker pull prefecthq/server
+docker pull prefecthq/ui
+docker pull prefecthq/apollo
+docker pull hasura/graphql-engine
+docker pull dgraph/standalone:master
+docker pull dgraph/dgraph
 
 # Logging. I love logging
-podman pull graylog/graylog:3.3
-podman pull elasticsearch/elasticsearch-oss:7.9.3
-podman pull logstash/logstash-oss:7.9.3
-podman pull grafana/loki
-podman pull grafana/promtail
-podman pull grafana/grafana
+docker pull graylog/graylog
+docker pull elasticsearch/elasticsearch-oss:8.4.1
+docker pull logstash/logstash-oss:8.4.1
+docker pull grafana/loki
+docker pull grafana/promtail
+docker pull grafana/grafana
 
 # because sometimes I have to fake AWS
-podman pull minio/minio
+docker pull minio/minio
 
 # For no work reason
-podman pull linuxserver/musicbrainz
+docker pull linuxserver/musicbrainz
 
 # For pipeline and data workdflow
-podman pull apache/nifi-registry
-podman pull apache/nifi
+docker pull apache/nifi-registry
+docker pull apache/nifi
 
 # If caching/mirroring is setup right, I should feel perfectly okay doing the following:
-# podman system prune --all --force --volumes
+# docker system prune --all --force --volumes
